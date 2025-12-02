7d85096f1c7fc63541e3a5878413758880e632a2
zanadoman <domanseven@gmail.com>
2025-12-02 16:04:02 +0100

    day 2 solution

--- src/day1.rs
+++ src/day1.rs
@@ -3,20 +3,17 @@
 #![warn(clippy::nursery, clippy::pedantic)]
 
 fn main() {
-    let input = aoc25::read_input(env!("CARGO_BIN_NAME")).expect("input");
     let mut dial = 50;
     let mut password = 0;
 
-    for line in &input {
+    for line in aoc25::read_input(env!("CARGO_BIN_NAME")).expect("input") {
         let old = dial;
-        let value = line[1..].parse::<i128>().expect("value");
-        if line.starts_with('L') {
-            dial -= value;
-        } else if line.starts_with('R') {
-            dial += value;
-        } else {
-            panic!("Invalid direction");
-        }
+        let value: i128 = line[1..].parse().expect("value");
+        dial += match line.chars().next() {
+            Some('L') => -value,
+            Some('R') => value,
+            _ => panic!("Invalid direction"),
+        };
         password += (dial / 100 - old / 100).abs();
     }
 
---
+++ src/day2.rs
@@ -0,0 +1,27 @@
+#![forbid(unsafe_code)]
+#![deny(warnings)]
+#![warn(clippy::nursery, clippy::pedantic)]
+
+fn main() {
+    println!(
+        "Sum of invalid IDs: {}",
+        aoc25::read_input(env!("CARGO_BIN_NAME"))
+            .expect("input")
+            .iter()
+            .filter_map(|l| l.split_once('-'))
+            .map(|(l, h)| {
+                (l.parse().expect("l")..=h.parse().expect("h"))
+                    .filter(|v: &u128| {
+                        let s = v.to_string();
+                        (1..s.len()).filter(|v| s.len().is_multiple_of(*v)).any(
+                            |d| {
+                                (1..s.len() / d)
+                                    .all(|i| s[d * i..d * (i + 1)] == s[0..d])
+                            },
+                        )
+                    })
+                    .sum::<u128>()
+            })
+            .sum::<u128>()
+    );
+}
