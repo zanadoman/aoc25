5eab743b2d4a3c2084fbe08722e4093715f2e376
zanadoman <domanseven@gmail.com>
2025-12-06 15:42:28 +0100

    day 6 second part

--- src/day06.rs
+++ src/day06.rs
@@ -5,25 +5,58 @@
 
 fn main() {
     let mut input = aoc25::read_input(env!("CARGO_BIN_NAME")).expect("input");
-    let mut results: Vec<i128> = input
-        .remove(0)
-        .split_whitespace()
-        .map(|o| o.parse().expect("operand"))
-        .collect();
-    let operators = input.remove(input.len() - 1);
-    let operators: Vec<_> = operators.split_whitespace().collect();
+    let matrix: Vec<Vec<char>> =
+        input.iter().map(|l| l.chars().collect()).collect();
+    let mut exprs: Vec<String> = Vec::new();
 
-    for line in input {
-        for (i, operand) in line.split_whitespace().enumerate() {
-            let operand: i128 = operand.parse().expect("operand");
-            match operators[i] {
-                "+" => results[i] += operand,
-                "*" => results[i] *= operand,
-                _ => panic!("Invalid operator"),
+    for i in (0..matrix
+        .iter()
+        .max_by(|l, r| l.len().cmp(&r.len()))
+        .expect("max")
+        .len())
+        .rev()
+    {
+        let expr = matrix
+            .iter()
+            .filter_map(|r| {
+                r.get(i)
+                    .and_then(|c| if *c == ' ' { None } else { Some(c) })
+            })
+            .fold(String::new(), |s, c| format!("{s}{c}"))
+            .trim()
+            .to_owned();
+        if !expr.is_empty() {
+            let last = expr.chars().last().expect("last");
+            match last {
+                '+' | '*' => {
+                    exprs.push(expr[..expr.len() - 1].to_owned());
+                    exprs.push(last.to_string());
+                }
+                _ => exprs.push(expr),
             }
         }
     }
 
-    println!("{results:#?}");
+    let mut results = Vec::new();
+    let mut operands: Vec<i128> = Vec::new();
+
+    for expr in exprs {
+        match expr.as_str() {
+            "+" => {
+                results.push(operands.pop().expect("operand"));
+                while let Some(operand) = operands.pop() {
+                    *results.last_mut().expect("last") += operand;
+                }
+            }
+            "*" => {
+                results.push(operands.pop().expect("operand"));
+                while let Some(operand) = operands.pop() {
+                    *results.last_mut().expect("last") *= operand;
+                }
+            }
+            _ => operands.push(expr.parse().expect("operand")),
+        }
+    }
+
     println!("Result: {}", results.iter().sum::<i128>());
 }
